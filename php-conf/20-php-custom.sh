49f20e13aa9c6bfdfc7af5299e9adf06330aa82b
monta <monta@ednity.com>
2020-03-04 17:35:07 +0900

    fix docker-compose.yml for easy build

--- php-conf/20-php-custom.sh
+++ php-conf/20-php-custom.sh
@@ -7,6 +7,6 @@ cd /var/www/html/ && cp assets/fonts/notoserifjp/NotoSerifJP-Light.otf /usr/shar
   && cp assets/fonts/notoserifjp/NotoSerifJP-SemiBold.otf /usr/share/fonts/ \
   && fc-cache -fv
 
-wget https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64 \
+cd ~ && wget https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64 \
   && chmod +x mhsendmail_linux_amd64 \
   && mv mhsendmail_linux_amd64 /usr/local/bin/mhsendmail
