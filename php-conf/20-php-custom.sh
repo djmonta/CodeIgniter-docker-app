2548aeb1a7c98297901b64ceab37c12e6ef0e4e7
monta <monta@ednity.com>
2020-03-04 17:55:34 +0900

    change entrypoint -> bootstrap

--- php-conf/20-php-custom.sh
+++ php-conf/20-php-custom.sh
@@ -1,7 +1,8 @@
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-0.12.5-1.centos7.x86_64.rpm \
   && yum install -y --disablerepo=blackfire libXext.x86_64 libXrender xorg-x11-fonts-75dpi xorg-x11-fonts-Type1 ipa-gothic-fonts ipa-mincho-fonts ipa-pgothic-fonts ipa-pmincho-fonts \
   && rpm -ivh wkhtmltox-0.12.5-1.centos7.x86_64.rpm \
-  && rm -f wkhtmltox-0.12.5-1.centos7.x86_64.rpm
+  && rm -f wkhtmltox-0.12.5-1.centos7.x86_64.rpm \
+  && yum clean all
 
 cd /var/www/html/ && cp assets/fonts/notoserifjp/NotoSerifJP-Light.otf /usr/share/fonts/ \
   && cp assets/fonts/notoserifjp/NotoSerifJP-SemiBold.otf /usr/share/fonts/ \
